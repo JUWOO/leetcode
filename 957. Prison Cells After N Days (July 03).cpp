b143e924c0c1b2094c74bc27683cc469228010e0
JUWOO <dearjuwoo@gmail.com>
2020-07-06 22:48:06 +0900

    yet

--- 957. Prison Cells After N Days (July 03).cpp
+++ 957. Prison Cells After N Days (July 03).cpp
@@ -12,29 +12,11 @@ public:
         int i=0, j=0;
         for(i=0 ; i<8 ; i++)
             tmp[i] = cells[i];
-        while(index==0)
-        {
-            count++;
-            new_cells[0] = 0;
-            new_cells[7] = 0;
-            for(i=1 ; i<7 ; i++)
-            {
-                if(cells[i-1]==cells[i+1])
-                    new_cells[i] = 1;
-                else
-                    new_cells[i] = 0;
-            }
-            for(i=0 ; i<8 ; i++)
-                cells[i] = new_cells[i];
-            i=0;
-            while(tmp[i]==new_cells[i] && i<8)
-            {
-                i++;
-            }
-            if(i==8)
-                index = 1;
-        }
-        num = N % count;
+        num = N % 14;
+        if(num==0)
+            num=14;
+        for(i=0 ; i<8 ; i++)
+                cells[i] = tmp[i];
         while(num>0)
         {
             num--;
