4aeb1505dad844ab79217707a37b2041ecb2bad9
JUWOO <dearjuwoo@gmail.com>
2020-07-11 03:09:13 +0900

    200711

--- 2020 Summer/07. Island Parameter.cpp
+++ 2020 Summer/07. Island Parameter.cpp
@@ -1,3 +1,30 @@
+//https://leetcode.com/problems/island-perimeter/
+//Time Complexity : O(N*M)
+//Space Complexity : O(1)
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int count = 0;
+        int row = grid.size();
+        int col = grid[0].size();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    count += 4;
+                    if (i < row - 1)
+                        count -= 2 * grid[i + 1][j];
+                    if (j < col - 1)
+                        count -= 2 * grid[i][j + 1];
+                }
+            }
+        }
+        return count;
+    }
+};
+
+
+//Time complexity : O(2N*3)
+/*
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
@@ -10,11 +37,15 @@ public:
         //case row
         for(i=0; i<row; i++)
         {
-            for(int j=0; j<col-1; j++)
+            if(col==1){
+                j = 0;
+                if(grid[i][j]==1)
+                    count++;}
+            for(j=0; j<col-1; j++)
             {
                 if(j==0 && grid[i][j]==1)
                     count++;
-                else if(grid[i][j] != grid[i][j+1])
+                if(grid[i][j] != grid[i][j+1])
                     count++;
             }
             if(j==col-1 && grid[i][j]==1)
@@ -24,11 +55,15 @@ public:
         //case column
         for(j=0; j<col; j++)
         {
+            if(row==1){
+                i = 0;
+                if(i==0 && grid[i][j]==1)
+                    count++;}
             for(i=0; i<row-1; i++)
             {
                 if(i==0 && grid[i][j]==1)
                     count++;
-                else if(grid[i][j] != grid[i+1][j])
+                if(grid[i][j] != grid[i+1][j])
                     count++;
             }
             if(i==row-1 && grid[i][j]==1)
@@ -38,3 +73,4 @@ public:
         return count;        
     }
 };
+*/
